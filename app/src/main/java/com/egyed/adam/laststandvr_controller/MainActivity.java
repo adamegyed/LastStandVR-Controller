bb3a2dd41889b9d3ba99589b1af1b23b77fc283f
Rithik <Jain>
2017-03-25 20:48:54 -0700

    Jedis Implementation

--- app/src/main/java/com/egyed/adam/laststandvr_controller/MainActivity.java
+++ app/src/main/java/com/egyed/adam/laststandvr_controller/MainActivity.java
@@ -3,11 +3,20 @@ package com.egyed.adam.laststandvr_controller;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import redis.clients.jedis.Jedis;
+
+public class MainActivity extends AppCompatActivity implements Runnable {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Thread thread = new Thread();
+        thread.start();
+    }
+
+    public void run() {
+        JedisManager jedisManager = new JedisManager();
     }
 }
